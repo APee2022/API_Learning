6638c7bbb1d6b4f58ab3162ea15cd13ac50e0087
APee2022 <atulprakash869@gmail.com>
2024-05-02 18:17:09 +0530

    update the /check router

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ const MensRanking = require("../src/models/mens");
 const router = require("./routers/men");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.use(express.json());
 app.use(router);
+
+// ####################################################################
+// we made some update here now we are going to push this on github.
+app.get("/check", (req, res) => {
+  res.send("learn about how to push code in Github");
+});
+// ####################################################################
 
 app.listen(port, () => {
   console.log(`connection is live at port no. ${port}`);
